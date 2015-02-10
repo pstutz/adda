768fcdd1a48ada78600b20e652455958ae460763
Akanksha Singh <akanksa.singh@ihealthtechnologies.com>
2015-02-10 13:41:00 +0530

    adda: adding test case for Adda class

---
+++ src/test/scala/com/adda/AddaTest.scala
@@ -0,0 +1,24 @@
+package com.adda
+
+/**
+ * Created by asingh on 10/02/15.
+ */
+
+import akka.stream.scaladsl.{Sink, Source}
+import org.scalatest._
+
+class AddaTest extends FlatSpec {
+  "Adda" should "create Source of appropriate type" in {
+    val adda = new Adda
+    val source = adda.subscribeToSource[List[String]]
+    assert(source.isInstanceOf[Source[List[String]]])
+  }
+
+  "Adda" should "create Sink of appropriate type" in {
+    val adda = new Adda
+    val sink = adda.getPublicationSink[String]
+    assert(sink.isInstanceOf[Sink[String]])
+  }
+
+
+}
