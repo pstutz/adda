0ffceeaf6f8e933b3018f5e89ac7d24395713112
Philip Stutz <philip.stutz@gmail.com>
2015-02-10 10:09:47 +0100

    Added todos to test.

--- src/test/scala/com/adda/AddaTest.scala
+++ src/test/scala/com/adda/AddaTest.scala
@@ -1,19 +1,20 @@
 package com.adda
 
-/**
- * Created by asingh on 10/02/15.
- */
+import org.scalatest.FlatSpec
 
-import akka.stream.scaladsl.{Sink, Source}
-import org.scalatest._
+import akka.stream.scaladsl.Sink
+import akka.stream.scaladsl.Source
 
 class AddaTest extends FlatSpec {
+
+  // TODO(akanksha): This test cannot fail, because the generic type parameter List[String] is erased at runtime. 
   "Adda" should "create Source of appropriate type" in {
     val adda = new Adda
     val source = adda.subscribeToSource[List[String]]
     assert(source.isInstanceOf[Source[List[String]]])
   }
 
+  // TODO(akanksha): This test cannot fail, because the generic type parameter String is erased at runtime.
   "Adda" should "create Sink of appropriate type" in {
     val adda = new Adda
     val sink = adda.getPublicationSink[String]
