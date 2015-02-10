c47b1d1a7fdca9c30f7846c7bdf1c9fe237172cb
Jahangir Mohammed <md.jahangir27@gmail.com>
2015-02-10 22:01:23 +0530

    Fix import of Triple.

--- src/main/scala/com/adda/adapters/SesameAdapter.scala
+++ src/main/scala/com/adda/adapters/SesameAdapter.scala
@@ -3,9 +3,8 @@ package com.adda.adapters
 import org.openrdf.query.QueryLanguage
 import org.openrdf.repository.sail.SailRepository
 import org.openrdf.sail.memory.MemoryStore
-
 import com.adda.interfaces.TripleStore
-import com.adda.messages.Triple
+import com.adda.interfaces.Triple
 
 class SesameAdapter extends TripleStore {
 
