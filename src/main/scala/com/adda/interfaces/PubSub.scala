0f4cf4b0d5c737dcdbf13ddae457f69634c6d815
Philip Stutz <philip.stutz@gmail.com>
2015-02-10 12:35:47 +0100

    Added comments to PubSub interface.

--- src/main/scala/com/adda/interfaces/PubSub.scala
+++ src/main/scala/com/adda/interfaces/PubSub.scala
@@ -6,9 +6,17 @@ import akka.stream.scaladsl.Source
 import scala.reflect.ClassTag
 
 trait PubSub {
-  
+
+  /**
+   * Returns an Akka Streams source that is subscribed to all published objects of class `C'.
+   * Does not publish subclasses of `C'.
+   */
   def subscribeToSource[C: ClassTag]: Source[C]
 
+  /**
+   * Returns an Akka Streams sink that allows to publish objects of class `C'.
+   * Does not allow publishing subclasses of `C'.
+   */
   def getPublicationSink[C]: Sink[C]
 
 }
