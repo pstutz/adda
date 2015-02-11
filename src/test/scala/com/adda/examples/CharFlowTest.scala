0324d273977520066e921f98163f7094318b99af
Jahangir Mohammed <md.jahangir27@gmail.com>
2015-02-11 16:23:48 +0530

    CharFlowTest.

---
+++ src/test/scala/com/adda/examples/CharFlowTest.scala
@@ -0,0 +1,46 @@
+package com.adda.examples
+
+import akka.actor.ActorSystem
+import akka.stream.ActorFlowMaterializer
+import akka.stream.scaladsl.{ForeachSink, Flow, Source}
+import com.adda.Adda
+import org.scalatest.{FlatSpec, Matchers}
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
+
+/**
+ * Created by jahangirmohammed on 2/11/15.
+ */
+class CharFlowTest extends FlatSpec with Matchers {
+
+  "Adda" should "take a source of lower case characters and convert them into upper case characters" in {
+
+    val adda = new Adda
+    implicit val system = ActorSystem("Test")
+    implicit val materializer = ActorFlowMaterializer()
+
+    val lowerCaseToUpperFlow: Flow[Char, Char] = Flow[Char].map(f => {
+      f.toUpper
+    })
+
+   val pub = adda.getSource[Char]
+    
+    Source(List('a','d','d','a')).runWith(adda.getSink[Char])
+
+   val futureString =  pub.via(lowerCaseToUpperFlow).runFold(""){(x,y) => x+y}
+
+    val finalStringValue = Await.result(futureString, 5.seconds)
+
+    Thread.sleep(5000)
+
+    finalStringValue should be ("ADDA")
+
+    shutdown()
+
+    def shutdown() {
+      adda.shutdown()
+      system.shutdown()
+    }
+
+  }
+}
