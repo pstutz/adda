8e7f2ffd3707db192b7ed7826c9234fd305ebd1c
Philip Stutz <philip.stutz@gmail.com>
2015-02-11 17:14:22 +0100

    Removed implementations from trait.
    Hard to test, even with mocking.

--- src/main/scala/com/adda/interfaces/TripleStore.scala
+++ src/main/scala/com/adda/interfaces/TripleStore.scala
@@ -8,23 +8,9 @@ trait TripleStore extends SparqlSelect {
   def addTriple(t: Triple)
 
   /**
-   * Add a triple with subject, predicate and object to the store. By default
-   * delegates to the Triple object implementation, but can be overridden to
-   * save the Triple wrapper.
+   * Adds all triples in the iterator to the store.
    */
-  def addTriple(s: String, p: String, o: String) {
-    addTriple(Triple(s, p, o))
-  }
-
-  /**
-   * Adds all triples in the iterator. By default delegates to addTriple,
-   * but using this call allows the implementation to optimize bulk-loading.
-   */
-  def addTriples(triples: Iterator[Triple]) {
-    for (t <- triples) {
-      addTriple(t)
-    }
-  }
+  def addTriples(triples: Iterator[Triple])
 
   /**
    * Shuts down the store.
