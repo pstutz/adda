be0c2b75c2c584109bbd7835c6b21afbec503f27
Philip Stutz <philip.stutz@gmail.com>
2015-03-11 17:48:01 +0100

    Added a test for Adda handlers.

---
+++ src/test/scala/com/adda/HandlerTest.scala
@@ -0,0 +1,49 @@
+package com.adda
+
+import java.util.concurrent.atomic.AtomicInteger
+
+import org.scalatest.{ FlatSpec, Matchers }
+
+import akka.actor.ActorSystem
+import akka.stream.ActorFlowMaterializer
+import akka.stream.scaladsl.{ Sink, Source }
+import akka.stream.testkit.StreamTestKit
+
+class HandlerTest extends FlatSpec with Matchers {
+
+  "A privileged handler" should "handler all published entities" in {
+
+    val counter = new AtomicInteger
+    def intHandler(e: Any) = {
+      e match {
+        case i: Int => counter.addAndGet(i)
+        case other  => throw new Exception("Unexpected entry.")
+      }
+    }
+
+    val adda = new Adda(List(intHandler))
+    implicit val system = ActorSystem("Test")
+    implicit val materializer = ActorFlowMaterializer()
+
+    val elements = 1000000
+
+    val probe = StreamTestKit.SubscriberProbe[Int]
+    val in = Source(1 to elements).to(adda.getSink[Int])
+    val out = adda.getSource[Int].to(Sink(probe))
+    out.run
+    in.run
+
+    probe.expectSubscription().request(elements)
+    for (i <- 1 to elements) {
+      probe.expectNext(i)
+    }
+    probe.expectComplete()
+
+    val entrySum = (1 to elements).sum
+    counter.get should be(entrySum)
+
+    adda.awaitCompleted()
+    adda.shutdown()
+
+  }
+}
