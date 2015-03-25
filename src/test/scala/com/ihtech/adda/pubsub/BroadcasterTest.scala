a8e93dd0cf03d4e4aa9bfaa01198596e183c22fb
Philip Stutz <philip.stutz@gmail.com>
2015-03-25 20:32:18 +0100

    Cleanup and Codacy fixes.

--- src/test/scala/com/ihtech/adda/pubsub/BroadcasterTest.scala
+++ src/test/scala/com/ihtech/adda/pubsub/BroadcasterTest.scala
@@ -1,17 +1,14 @@
 package com.ihtech.adda.pubsub
 
 import scala.collection.immutable.Queue
-import scala.concurrent.duration.DurationInt
 
 import org.scalatest.{ BeforeAndAfterAll, Finders, FlatSpec, Matchers }
 
 import com.typesafe.config.ConfigFactory
 
 import akka.actor.{ ActorSystem, Props }
-import akka.pattern.ask
 import akka.stream.actor.ActorSubscriberMessage.OnNext
 import akka.testkit.{ EventFilter, TestActorRef }
-import akka.util.Timeout
 
 class BroadcasterTest extends FlatSpec with Matchers with BeforeAndAfterAll {
 
@@ -24,7 +21,6 @@ akka.loggers = ["akka.testkit.TestEventListener"]
   }
 
   private[this] val testStreamElement = OnNext("test")
-  implicit val timeout: Timeout = Timeout(300.seconds)
 
   private[this] val failingHandler: Any => Unit = { a: Any =>
     throw TestException("The handler had a problem.")
@@ -32,10 +28,8 @@ akka.loggers = ["akka.testkit.TestEventListener"]
 
   "Broadcaster actor" should "throw exception when the single-element handler fails" in {
     val broadcaster = TestActorRef(Props(new Broadcaster(List(failingHandler))))
-    val stringPublisher = broadcaster ? CreatePublisher[String]
     EventFilter[TestException](occurrences = 1) intercept {
       broadcaster ! testStreamElement
-      Thread.sleep(200)
     }
   }
 
@@ -43,7 +37,6 @@ akka.loggers = ["akka.testkit.TestEventListener"]
     val broadcaster = TestActorRef(Props(new Broadcaster(List(failingHandler))))
     EventFilter[TestException](occurrences = 1) intercept {
       broadcaster ! Queue[Any](testStreamElement.element, testStreamElement.element)
-      Thread.sleep(200)
     }
   }
 
