2aa1ef9b8172e6f3eb0b812ff0ad6b7ad3b4a7ce
Mykel Alvis <mykel.alvis@gmail.com>
2015-12-14 06:02:03 -0600

    Push to latest parent to replace license info

--- src/test/scala/com/ihealthtechnologies/adda/DelayingHandler.scala
+++ src/test/scala/com/ihealthtechnologies/adda/DelayingHandler.scala
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) ${project.inceptionYear} Mycila (mathieu.carbou@gmail.com)
+ * Copyright (C) 2015 Cotiviti Labs (nexgen.admin@cotiviti.io)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
